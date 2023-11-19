c74fc6a2c77a5bbdf6acd094281bc70b48d7b7b1
domini04 <107534459+domini04@users.noreply.github.com>
2023-11-19 20:40:27 +0900

    d

--- mask-front/src/api/mask/getTop3.js
+++ mask-front/src/api/mask/getTop3.js
@@ -1,9 +1,10 @@
 import React from "react";
 import axios from "axios";
 
-export const getTop3 = async ({ setTopMask }) => {
+export const getTop3 = async ({ setTopMask }) => { //setTopMask는 MainPage.js에서 useState로 선언한 topMask를 set해주는 함수
   // const response = await axios.get("http://localhost:8080/mask/top3");
-  const response = await axios.get("http://35.216.122.45:8080/mask/top3");
+  const response = await axios.get("http://34.64.75.217:8080/health");
+  console.log(response.data);
 
   // console.log(response.data);
   setTopMask(response.data.result);
